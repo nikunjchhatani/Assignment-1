a981933f8d9f176dd3a805675a834b52371d3b59
nikunjchhatani <nikunjchhatani@gmail.com>
2024-02-17 09:31:35 +0530

    requirement 2

--- requirements 2/readFile.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-fs.readFile('poem.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading the file:', err);
-    return;
-  }
-  console.log(data);
-});
